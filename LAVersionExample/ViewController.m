ae6826093b3a667e6e490d94fca71c972b207bf0
Luis <luis@jig.es>
2012-12-27 11:21:14 +0100

    Initial Commit

---
+++ LAVersionExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LAVersionExample
+//
+//  Created by Mac2-Get-app on 27/12/12.
+//  Copyright (c) 2012 Luis Ascorbe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LAVersionExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LAVersionExample
+//
+//  Created by Mac2-Get-app on 27/12/12.
+//  Copyright (c) 2012 Luis Ascorbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
